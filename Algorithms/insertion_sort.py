f5d9b64631729294c33a0cdcb535764c57534bc8
Ling Chen <lche0147@student.monash.edu>
2024-03-16 23:46:15 +1100

    fixed insertion sort

--- Algorithms/insertion_sort.py
+++ Algorithms/insertion_sort.py
@@ -3,12 +3,14 @@ def insertion_sort(nums):
     # idea: shuffle(insert) the current element to the correct position at each iteration
     # Each iteration sorts 1 element 
 
-    for i in range(len(nums)):
+    for i in range(1, len(nums)):
         # arr is sorted for idx 0 -> i-1
-        j = i
-        while j>0 and nums[j] < nums[j-1]:
-            nums[j], nums[j-1] = nums[j-1], nums[j]
+        key = nums[i]
+        j = i - 1
+        while j>=0 and nums[j] > key:
+            nums[j+1] = nums[j] 
             j -= 1
+        nums[j+1] = key
         # arr is sorted for idx 0 -> i
         # i = i + 1
     return None # sorted in-place
