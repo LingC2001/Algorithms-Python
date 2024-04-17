b398b85049d866e710c067e77a2bddd1714aad90
Ling Chen <lche0147@student.monash.edu>
2024-04-17 22:20:29 +1000

    complexity analysis for connected components, and changed to adj_list implementation

--- Algorithms/Graphs/connected_components.py
+++ Algorithms/Graphs/connected_components.py
@@ -1,6 +1,19 @@
+"""
+Connected components complexity analysis:
 
+We just perofrm dfs until all nodes are visited. Therefore has the same complexity as dfs.
 
-def connected_components(adj_matrix):
+Time Complexity:
+    Using an adjacency list: O(V + E) for directed and O(V + 2E) for undirected graphs
+    Using an adjacency matrix: O(V^2), since V columns are checked for each Vertex.
+
+Space complexity
+    In worst case the graph is a striaght line, therefore:
+    Worst case aux space: O(V), not counting adj_matrix or adj_list used
+
+"""
+
+def connected_components(adj_list):
     """
     Notice how each starting dfs call visits exactly one connected component.
     Therefore the number of connected components is just the number of dfs we need to perform
@@ -9,20 +22,21 @@ def connected_components(adj_matrix):
     Instead of keeping a visited list, and having a separate components list, we will combine them
     and just have a components list that has initial values of None
     """
-    def dfs(vertex):
-        components[vertex] = num_components
-        
-        for i in range(len(adj_matrix[vertex])):
-            if adj_matrix[vertex][i] == 1 and components[i] is None:
-                dfs(i)
 
-    n = len(adj_matrix)
+    n = len(adj_list)
     components = [None]*n
-    num_components = 0
+    component_num = 0
 
     for i in range(n):
         if components[i] is None:
-            num_components += 1
-            dfs(i)
+            component_num += 1
+            dfs(adj_list, i, components, component_num)
 
-    return num_components, components
+    return component_num, components
+
+def dfs(adj_list, vertex, components, component_num):
+        components[vertex] = component_num
+        
+        for e in adj_list[vertex]:
+            if components[e] is None:
+                dfs(adj_list, e, components, component_num)
--- Algorithms/Graphs/tests/connected_components_test.py
+++ Algorithms/Graphs/tests/connected_components_test.py
@@ -16,7 +16,12 @@ def test_1():
                   [1, 0, 0, 0, 0],
                   [1, 0, 0, 0, 0],
                   [1, 0, 0, 0, 0]]
-    res = connected_components(adj_matrix)
+    adj_list = [[1, 2, 3, 4],
+                [0],
+                [0],
+                [0],
+                [0]]
+    res = connected_components(adj_list)
     assert res == (1, [1, 1, 1, 1, 1])
 
 def test_2():
@@ -25,7 +30,12 @@ def test_2():
                   [1, 0, 0, 1, 0],
                   [0, 1, 1, 0, 0],
                   [0, 0, 0, 0, 0]]
-    res = connected_components(adj_matrix)
+    adj_list = [[1, 2],
+                [0, 3],
+                [0, 3],
+                [1, 2],
+                []]
+    res = connected_components(adj_list)
     assert res == (2, [1, 1, 1, 1, 2])
 
 def test_3():
@@ -34,7 +44,12 @@ def test_3():
                   [0, 0, 0, 0, 0],
                   [0, 1, 0, 0, 1],
                   [0, 0, 0, 1, 0]]
-    res = connected_components(adj_matrix)
+    adj_list = [[1],
+                [0, 3],
+                [],
+                [1, 4],
+                [3]]
+    res = connected_components(adj_list)
     assert res == (2, [1, 1, 2, 1, 1])
 
 def test_4():
@@ -43,5 +58,10 @@ def test_4():
                   [1, 0, 0, 0, 0],
                   [0, 0, 0, 0, 1],
                   [0, 0, 0, 1, 0]]
-    res = connected_components(adj_matrix)
+    adj_list = [[1,2],
+                [0],
+                [0],
+                [4],
+                [3]]
+    res = connected_components(adj_list)
     assert res == (2, [1, 1, 1, 2, 2])
