ec067ed9613830659e424bc115cb48961ee69a7f
Ling Chen <lche0147@student.monash.edu>
2024-04-18 00:47:51 +1000

    complexity analysis for kahn's algorithm

--- Algorithms/Graphs/tests/topological_sort_khan_test.py
+++ Algorithms/Graphs/tests/topological_sort_khan_test.py
@@ -6,7 +6,8 @@ import pytest
 
 def test_empty():
     adj_matrix = []
-    res = topological_sort_khan(adj_matrix)
+    adj_list = []
+    res = topological_sort_khan(adj_list)
     assert res == []
 
 def test_line_graph():
@@ -20,7 +21,16 @@ def test_line_graph():
                   [0, 0, 0, 0, 0, 1, 0],
                   [0, 0, 0, 0, 0, 0, 1],
                   [0, 0, 0, 0, 0, 0, 0]]
-    res = topological_sort_khan(adj_matrix)
+    
+    adj_list = [[1],
+                [2],
+                [4],
+                [0],
+                [5],
+                [6],
+                []]
+
+    res = topological_sort_khan(adj_list)
     assert res == [3, 0, 1, 2, 4, 5, 6]
 
 def test_two_lines():
@@ -35,7 +45,15 @@ def test_two_lines():
                   [0, 0, 0, 0, 0, 0, 0],
                   [0, 0, 0, 0, 0, 0, 1],
                   [0, 0, 0, 0, 0, 0, 0]]
-    res = topological_sort_khan(adj_matrix)
+    adj_list = [[1],
+                [2],
+                [4],
+                [0],
+                [],
+                [6],
+                []]
+    
+    res = topological_sort_khan(adj_list)
     assert res == [3, 5, 0, 6, 1, 2, 4]
 
 def test_not_connected():
@@ -46,7 +64,14 @@ def test_not_connected():
                   [0, 0, 0, 0, 0, 0, 0],
                   [0, 0, 0, 0, 0, 0, 0],
                   [0, 0, 0, 0, 0, 0, 0]]
-    res = topological_sort_khan(adj_matrix)
+    adj_list = [[],
+                [],
+                [],
+                [],
+                [],
+                [],
+                []]
+    res = topological_sort_khan(adj_list)
     assert res == [0, 1, 2, 3, 4, 5, 6]
 
 def test_general():
@@ -65,5 +90,14 @@ def test_general():
                   [0, 0, 0, 0, 0, 1, 0],
                   [0, 0, 0, 0, 0, 0, 1],
                   [0, 0, 0, 0, 0, 0, 0]]
-    res = topological_sort_khan(adj_matrix)
+    
+    adj_list = [[2],
+                [2],
+                [6],
+                [4],
+                [5],
+                [6],
+                []]
+    
+    res = topological_sort_khan(adj_list)
     assert res == [0, 1, 3, 2, 4, 5, 6]
--- Algorithms/Graphs/topological_sort_khan.py
+++ Algorithms/Graphs/topological_sort_khan.py
@@ -1,12 +1,45 @@
+"""
+Kahn's algorithm for topological sorting
+
+Given a DAG (directed acyclic graph)
+Notice how in topological order, we start with the nodes where there are no incoming edges
+and end on nodes with no outgoing edges.
+Kahn's algorithm uses this fact and maintains a queue of vertices ready to be inserted into 
+the sorted list. A vertex is considered ready when it has no incoming edges. When a vertex is
+inserted, all outgoing edges of it are removed, so that the next item in topological order
+would have no more incoming edges.
+
+Note that the in_degree (number of incoming edges) of each node can be calculated,
+and when an incoming edge is removed, reduce the in_degree by 1. This is more efficient
+than actually removing the edge in the graph.
+
+Time Complexity:
+    Every vertex is process once, and all the outgoing edges of the vertex are checked and removed.
+    This is O(V + E) when using adjacency list, and O(V^2) when using adjacency matrix
+
+    For the initial indegree calculation of every node, every edge is checked.
+    Therefore O(V) for creating V-size zero list, O(E) checking every edge, using adj_list
+    If using adj_matrix, O(V) for creating V-size zero list, O(V^2) for checking every edge
+
+    Combined:
+        adj_list: O(2V + 2E)
+        adj_matrix: O(V + 2V^2)
+
+Space complexity:
+    O(V) for order, queue and in_degree
+
+"""
+
+
 from collections import deque
 
-def topological_sort_khan(adj_matrix):
+def topological_sort_khan(adj_list):
     """
     given a directed acyclic graph
     """
     order = []
     ready = deque()
-    in_degree = calculate_in_degree(adj_matrix)
+    in_degree = calculate_in_degree(adj_list)
     # add all vertices with no incoming edges
     
     for i in range(len(in_degree)):
@@ -17,22 +50,20 @@ def topological_sort_khan(adj_matrix):
     while ready:
         u = ready.popleft()
         order.append(u)
-        for v in range(len(adj_matrix[u])):
-            if adj_matrix[u][v] == 1:
-                # remove edge (don't need to actually delete it if using in degree method)
-                in_degree[v] -= 1
+        for v in adj_list[u]:
+            # remove edge (don't need to actually delete it if using in degree method)
+            in_degree[v] -= 1
 
-                if in_degree[v] == 0:    
-                    ready.append(v)
+            if in_degree[v] == 0:    
+                ready.append(v)
                     
     return order
 
-def calculate_in_degree(adj_matrix):
-    n = len(adj_matrix)
+def calculate_in_degree(adj_list):
+    n = len(adj_list)
     in_degree = [0] * n
 
-    for u in range(n):
-        for v in range(n):
-            if adj_matrix[u][v] == 1:
-                in_degree[v] += 1
+    for i in range(n):
+        for v in adj_list[i]:
+            in_degree[v] += 1
     return in_degree
