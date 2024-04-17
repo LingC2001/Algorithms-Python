4cbd98d1b43ffed796c1801e09093b5acb3300d0
Ling Chen <lche0147@student.monash.edu>
2024-04-18 01:25:29 +1000

    complexity analysis for union find disjoint set

--- Algorithms/Graphs/union_find.py
+++ Algorithms/Graphs/union_find.py
@@ -1,3 +1,39 @@
+"""
+Union Find Disjoint Set Complexity analysis
+
+Time complexity:
+    Find operation:
+        It keeps going up the parent until we reach the root node, which has parent equal to itself.
+        In the worst case, when it is a straight line, it could be O(n)
+
+        However with path compression optimisation, where whenever we perform a find operation,
+        all nodes along the path is connected directly to the root, so that next time finding which set
+        one of the nodes belongs to would be O(1). On average this makes m operations take O(mlog(n)) time,
+        which means each operation taking O(log(n)) time.
+
+    Union operation:
+        Notice that in the union operation, we first perform 2 find operations.
+        We can also optimise the union operation by doing "Union by Rank" where the set 
+        with a smaller rank is connected to the root of the set with the bigger rank. A rank
+        of a set is the upper bound of it height/depth, since we don't update the rank when doing
+        path compression, that means the actual height may be smaller than its rank.
+        This creates more balanced trees, which again makes m operations take O(mlog(n)) time, 
+        which means each operation taking O(log(n)) time. 
+
+        There is also "Union by size" where the set with smaller number of nodes in it is 
+        connected with the larger set.
+    
+    Combined together:
+        It can be proved that with both of the above optimisations, the worst case time complexity of
+        a sequence of m operations is O(m*alpha(n)), where alpha(n) is the inverse Ackermann function,
+        an extremely slow growing function. Functionally this means each operation is O(1), which is
+        alot better than O(log(n)).
+    
+Space complexity:
+    O(n) for the parent list, and rank list.
+    Also the recursion depth of the find function, which was shown to be O(log(n))
+"""
+
 
 class UnionFind:
     def __init__(self, n):
