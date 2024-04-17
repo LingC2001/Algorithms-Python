3f92a5fdb496307e09bf6ce593128666ca9e4a61
Ling Chen <lche0147@student.monash.edu>
2024-04-17 21:53:15 +1000

    complexity analysis for dfs, and changed to adj_list implementation

--- Algorithms/Graphs/depth_first_search.py
+++ Algorithms/Graphs/depth_first_search.py
@@ -1,20 +1,35 @@
+"""
+DFS complexity analysis
 
-def depth_first_search(adj_matrix):
+Each vertex is visited once, and all the outgoing edges of the vertex are checked.
 
-    def dfs(vertex):
-        visited[vertex] = True
-        res.append(vertex)
-        
-        for i in range(len(adj_matrix[vertex])):
-            if adj_matrix[vertex][i] == 1 and not visited[i]:
-                dfs(i)
+Time Complexity:
+    Using an adjacency list: O(V + E) for directed and O(V + 2E) for undirected graphs
+    Using an adjacency matrix: O(V^2), since V columns are checked for each Vertex.
 
-    n = len(adj_matrix)
+Space complexity
+    In worst case the graph is a striaght line, therefore:
+    Worst case aux space: O(V), not counting adj_matrix or adj_list used
+
+"""
+
+
+def depth_first_search(adj_list):
+
+    n = len(adj_list)
     visited = [False]*n
     res = []
 
     for i in range(n):
         if not visited[i]:
-            dfs(i)
+            dfs(adj_list, i, visited, res)
 
     return res
+
+def dfs(adj_list, vertex, visited, res):
+        visited[vertex] = True
+        res.append(vertex)
+        
+        for e in adj_list[vertex]:
+            if not visited[e]:
+                dfs(adj_list, e, visited, res)
--- Algorithms/Graphs/tests/depth_first_search_test.py
+++ Algorithms/Graphs/tests/depth_first_search_test.py
@@ -11,31 +11,49 @@ def test_empty():
 
 def test_1():
     # vertex 0 is connected to all other vetices
-    adj_matrix = [[0, 1, 1, 1, 1],
-                  [1, 0, 0, 0, 0],
-                  [1, 0, 0, 0, 0],
-                  [1, 0, 0, 0, 0],
-                  [1, 0, 0, 0, 0]]
-    res = depth_first_search(adj_matrix)
+    # adj_matrix = [[0, 1, 1, 1, 1],
+    #               [1, 0, 0, 0, 0],
+    #               [1, 0, 0, 0, 0],
+    #               [1, 0, 0, 0, 0],
+    #               [1, 0, 0, 0, 0]]
+    adj_list = [[1, 2, 3, 4],
+                [0],
+                [0],
+                [0],
+                [0]]
+    res = depth_first_search(adj_list)
     assert res == [0, 1, 2, 3, 4]
 
 def test_2():
     # vertex 0 is connected to 1, which is connected to 3, which is connected to 2
     # vertex 4 is not connected to anything but is still traversed since the algorithm
     # is designed to traverse all nodes even if they are not connected
-    adj_matrix = [[0, 1, 1, 0, 0],
-                  [1, 0, 0, 1, 0],
-                  [1, 0, 0, 1, 0],
-                  [0, 1, 1, 0, 0],
-                  [0, 0, 0, 0, 0]]
-    res = depth_first_search(adj_matrix)
+    # adj_matrix = [[0, 1, 1, 0, 0],
+    #               [1, 0, 0, 1, 0],
+    #               [1, 0, 0, 1, 0],
+    #               [0, 1, 1, 0, 0],
+    #               [0, 0, 0, 0, 0]]
+
+    adj_list = [[1, 2],
+                [0, 3],
+                [0, 3],
+                [1, 2],
+                []]
+    res = depth_first_search(adj_list)
     assert res == [0, 1, 3, 2, 4]
 
 def test_3():
-    adj_matrix = [[0, 1, 0, 0, 0],
-                  [1, 0, 0, 1, 0],
-                  [0, 0, 0, 0, 0],
-                  [0, 1, 0, 0, 1],
-                  [0, 0, 0, 1, 0]]
-    res = depth_first_search(adj_matrix)
+    # adj_matrix = [[0, 1, 0, 0, 0],
+    #               [1, 0, 0, 1, 0],
+    #               [0, 0, 0, 0, 0],
+    #               [0, 1, 0, 0, 1],
+    #               [0, 0, 0, 1, 0]]
+
+    adj_list = [[1],
+                [0, 3],
+                [],
+                [1, 4],
+                [3]]
+    
+    res = depth_first_search(adj_list)
     assert res == [0, 1, 3, 4, 2]
